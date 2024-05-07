a3e75da2e391c6259b4cb6855a9e4739251d5a6e
mohi3112 <rupindersingh0853@gmail.com>
2024-05-07 22:54:01 +0530

    migration added for roles and role user table

---
+++ database/migrations/2024_05_07_172008_create_roles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        DB::table('roles')->insert([
+            ['name' => 'superadmin', 'description' => 'Super Admin Role'],
+            ['name' => 'admin', 'description' => 'Admin Role'],
+            ['name' => 'user', 'description' => 'User Role'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
